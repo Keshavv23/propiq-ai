2615a6456729d397e459c8b610e069a4d1343b35
Keshavv23 <mryogiraj18@gmail.com>
2026-05-30 12:07:22 +0530

    Fix frontend submodule

--- frontend/app/api/index.ts
+++ frontend/app/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://propiq-ai.onrender.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://propiq-ai-eta.vercel.app",
 });
 
 export default API;
